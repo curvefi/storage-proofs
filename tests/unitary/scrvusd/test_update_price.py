acfd92be71268da756ac746b9139ea80bc5a3faa
heswithme <heswithme@pm.me>
2024-12-18 18:38:58 +0100

    test: proper unitary test. single one for now

--- tests/unitary/scrvusd/test_update_price.py
+++ tests/unitary/scrvusd/test_update_price.py
@@ -7,5 +7,4 @@ def test_update_price(curve_dao, oracle_v1, verifier_contract):
     params_array = [1, 0, 1, 0, 0, 0, 0]
     with boa.env.prank(verifier_contract):
         oracle_v1.update_price(params_array, ts)
-    # rewards_handler.eval(f"twa._take_snapshot({snapshot_amount})")
-    assert True
+    assert oracle_v1.eval("self.price_params") == params_array
