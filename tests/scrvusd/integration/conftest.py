f26525ab602524f1af8ed5fd159c9e1bac4e78e2
Roman Agureev <Agureev.RS@phystech.edu>
2025-06-17 18:16:37 +0300

    fix: tests

--- tests/scrvusd/integration/conftest.py
+++ tests/scrvusd/integration/conftest.py
@@ -1,2 +1,2 @@
-from tests.scrvusd.oracle.conftest import soracle, set_verifier  # noqa: F401  # reusing fixture
+from tests.scrvusd.oracle.conftest import soracle, set_verifier, initial_price  # noqa: F401  # reusing fixture
 from tests.scrvusd.verifier.conftest import verifier  # noqa: F401  # reusing fixture
--- tests/scrvusd/integration/test_verify.py
+++ tests/scrvusd/integration/test_verify.py
@@ -1,4 +1,4 @@
-from tests.scrvusd.verifier.unitary.test_price import (
+from tests.scrvusd.verifier.unitary.test_price_update import (
     scrvusd_slot_values,  # noqa: F401  # reusing tests
     test_by_blockhash as test_price_by_blockhash,  # noqa: F401  # reusing tests
     test_by_stateroot as test_price_by_stateroot,  # noqa: F401  # reusing tests
